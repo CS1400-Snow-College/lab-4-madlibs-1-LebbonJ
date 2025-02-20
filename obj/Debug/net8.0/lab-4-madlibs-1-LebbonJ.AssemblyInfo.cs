da6c822b64ca06a21626574c76ae3f873dfb06d1
LebbonJ <justin.lebbon@students.snow.edu>
2025-02-19 21:53:06 -0700

    final commit

--- obj/Debug/net8.0/lab-4-madlibs-1-LebbonJ.AssemblyInfo.cs
+++ obj/Debug/net8.0/lab-4-madlibs-1-LebbonJ.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("lab-4-madlibs-1-LebbonJ")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f9a3b15051ebefce6ac9f7d8400df0512eed91ba")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2102d04f2612fcc2f209896054374c82a5fd8420")]
 [assembly: System.Reflection.AssemblyProductAttribute("lab-4-madlibs-1-LebbonJ")]
 [assembly: System.Reflection.AssemblyTitleAttribute("lab-4-madlibs-1-LebbonJ")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
